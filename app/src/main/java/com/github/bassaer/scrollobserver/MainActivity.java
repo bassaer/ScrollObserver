dfcf3899d6a198a23e5da76eb37b0a4f72da44c7
Tsubasa Nakayama <app.nakayama@gmail.com>
2017-08-31 01:14:25 +0900

    Add function of getting scroll position

--- app/src/main/java/com/github/bassaer/scrollobserver/MainActivity.java
+++ app/src/main/java/com/github/bassaer/scrollobserver/MainActivity.java
@@ -2,23 +2,45 @@ package com.github.bassaer.scrollobserver;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private static final int LIST_SIZE = 20;
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = (ListView) findViewById(R.id.main_list_view);
+        mListView = (ListView) findViewById(R.id.main_list_view);
         CustomListAdapter adapter = new CustomListAdapter(this, 0, getListItem());
-        listView.setAdapter(adapter);
+        mListView.setAdapter(adapter);
+        mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                View item = mListView.getChildAt(0);
+                if (item != null) {
+                    int scrollY = -item.getTop() + mListView.getFirstVisiblePosition() * item.getHeight();
+                    Log.d(
+                            MainActivity.class.getSimpleName(),
+                            "ScrollY = " + scrollY
+                    );
+                }
+            }
+        });
     }
 
     private List<Object> getListItem() {
